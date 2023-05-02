2d5250f621be0612053e5902a7aeddf94a7b7164
srithanujan <srithanujan@gmail.com>
2023-05-02 17:38:38 +0530

    register-login-fr_end

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./components/Navbar";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link, RouterProvider } from "react-router-dom";
 import Homescreen from "./screens/Homescreen";
 import Requestscreen from "./screens/Requestscreen";
+import Registerscreen from "./screens/Registerscreen";
+import Loginscreen from "./screens/Loginscreen";
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
         <Routes>
         <Route path="/home" exact Component={Homescreen}/>
         <Route path='/book/:requestid' exact Component={Requestscreen} />
+        <Route path='/register' exact Component={Registerscreen}/>
+        <Route path='/login' exact Component={Loginscreen}/>
 
         </Routes>
       </BrowserRouter>
