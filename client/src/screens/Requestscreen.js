cd11b6a8d1f895c3817c3d4401a1ede39db0ddfa
srithanujan <srithanujan@gmail.com>
2023-05-02 14:50:35 +0530

    food-request-fr_end

--- client/src/screens/Requestscreen.js
+++ client/src/screens/Requestscreen.js
@@ -4,8 +4,8 @@ import axios from 'axios';
 
 function Requestscreen() {
   const [requests, setRequests] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState();
 
   const { requestid } = useParams();
 
@@ -27,18 +27,40 @@ function Requestscreen() {
   }, [requestid]);
 
   return (
-    <div>
-      <h1>Request screen</h1>
-      <h1>Request id = {requestid}</h1>
-      {loading && <p>Loading...</p>}
-      {error && <p>Error occurred while fetching request.</p>}
-      {requests && (
-        <div>
-          <p>Request details:</p>
-          <p>Request id: {requests.id}</p>
-          <p>Request status: {requests.status}</p>
+    <div className='m-5'>
+     {loading ? (<h1>Loading...</h1>): error ? (<h1>Error...</h1>) : (<div>
+      <div className='row justify-content-center mt-5 bs'>
+        <div className='col-md-6'>
+          <h1>{requests.name}</h1>
+          <img src={requests.imageurls[0]} className='bigimg' alt=''/>
         </div>
-      )}
+        <div className='col-md-6'>
+          <div style={{textAlign: 'right'}}>
+          <h1>Request Details</h1>
+          <hr/>
+          <b>
+          <p>Name : </p>
+          <p>From Date :</p>
+          <p>To Date :</p>
+          <p>Needed Foods : {requests.neededfoods}</p>
+          </b>
+          </div>
+          <div style={{textAlign: 'right'}}>
+            <b>
+            <h1>Amount</h1>
+            <hr/>
+            <p>Total Days: </p>
+            <p>Needed foods per day : {requests.neededfoods}</p>
+            <p>Total Needed Foods</p>
+            </b>
+          </div>
+          <div style={{float: "right"}}>
+            <button className='btn btn-primary'>Request Now</button>
+          </div>
+          
+        </div>
+      </div>
+     </div>)}
     </div>
   );
 }
